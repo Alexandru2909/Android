ddf8b7086c6cb4be8b71cdde75079f0058c71035
AlexandruPutanu <alexandruputanu>
2020-04-08 15:31:08 +0300

    Dissapointing end of laboratory 7 Can't capture camera ouput on textureview;

--- app/src/main/java/com/example/labapp/AddCar.java
+++ app/src/main/java/com/example/labapp/AddCar.java
@@ -2,7 +2,10 @@ package com.example.labapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddCar extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class AddCar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_car);
+        final Button button = (Button) findViewById(R.id.button4);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                takePhotoLauncher();
+            }
+        });
+    }
+    private void takePhotoLauncher(){
+        Intent intent = new Intent(this, takePhoto.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/labapp/takePhoto.java
+++ app/src/main/java/com/example/labapp/takePhoto.java
@@ -1,19 +1,104 @@
 package com.example.labapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Camera;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Size;
+import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
+import android.widget.ExpandableListAdapter;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class takePhoto extends AppCompatActivity {
+
+
     Button takePictureButton;
     TextureView textureView;
-    CameraDevice cam;
-    public void takePicture(){
-//        Take picture
+    CameraManager cameraManager;
+    Size previewSize;
+    String cameraId;
+    CameraDevice.StateCallback stateCallback;
+    CameraDevice cameraDevice;
+    private final TextureView.SurfaceTextureListener mSurfaceTextureListener
+            = new TextureView.SurfaceTextureListener() {
+
+
+
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture texture, int width, int height) {
+            getCam();
+            open();
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture texture, int width, int height) {
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture texture) {
+            return true;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture texture) {
+        }
+
+    };
+
+    public void takePicture(View view) {
+
+    }
+
+    private void getCam() {
+        try {
+
+            for (String cameraId : cameraManager.getCameraIdList()) {
+                CameraCharacteristics cameraCharacteristics =
+                        cameraManager.getCameraCharacteristics(cameraId);
+                if (cameraCharacteristics.get(CameraCharacteristics.LENS_FACING) ==
+                        CameraCharacteristics.LENS_FACING_BACK) {
+                    StreamConfigurationMap streamConfigurationMap = cameraCharacteristics.get(
+                            CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+                    previewSize = streamConfigurationMap.getOutputSizes(SurfaceTexture.class)[0];
+                    this.cameraId = cameraId;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private void open() {
+        try {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+                    == PackageManager.PERMISSION_GRANTED) {
+                cameraManager.openCamera(cameraId, stateCallback, null);
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -25,11 +110,38 @@ public class takePhoto extends AppCompatActivity {
 
         takePictureButton = (Button) findViewById(R.id.buttonPhoto);
         assert takePictureButton != null;
+
+        stateCallback = new CameraDevice.StateCallback() {
+            @Override
+            public void onOpened(CameraDevice cameraDevice) {
+                takePhoto.this.cameraDevice = cameraDevice;
+            }
+
+            @Override
+            public void onDisconnected(CameraDevice cameraDevice) {
+                cameraDevice.close();
+                takePhoto.this.cameraDevice = null;
+            }
+
+            @Override
+            public void onError(CameraDevice cameraDevice, int error) {
+                cameraDevice.close();
+                takePhoto.this.cameraDevice = null;
+            }
+        };
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            takePictureButton.setEnabled(false);
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
+        }
+        cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
         takePictureButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                takePicture();
+                takePicture(textureView);
             }
         });
+//        Doesn t work
     }
 }
